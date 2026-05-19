b16b76a7d1508d5377511603d8d1a820fd262f85
Ken Conner <kenconner@Alis-MacBook-Air.local>
2026-05-18 18:57:36 -0600

    Add verbose Xcode Cloud logging

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
+set -e
+set -x
 
-echo "Installing Node dependencies..."
+echo "=== CI POST CLONE START ==="
 
+pwd
+ls
+ls ios
+
+echo "=== Installing Node dependencies ==="
 npm install
 
-echo "Installing CocoaPods..."
-
+echo "=== Installing CocoaPods ==="
 cd ios
+
 pod install --repo-update
+
+echo "=== Listing generated xcconfig ==="
+ls "Pods/Target Support Files/Pods-WorkoutTracker"
+
+echo "=== CI POST CLONE COMPLETE ==="
