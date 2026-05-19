6c0cf49d1da8377ad50e993ef234e27cd53e8a83
Ken Conner <kenconner@Alis-MacBook-Air.local>
2026-05-18 20:20:19 -0600

    Fix Xcode Cloud node environment2

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -2,14 +2,24 @@
 
 set -e
 
-export NVM_DIR="$HOME/.nvm"
+export HOMEBREW_NO_INSTALL_CLEANUP=TRUE
 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+if [[ -d "$HOME/.nvm" ]]; then
+  export NVM_DIR="$HOME/.nvm"
+  . "$NVM_DIR/nvm.sh"
+fi
+
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+
+echo "Node version:"
+node --version
+
+echo "NPM version:"
+npm --version
 
 echo "Installing node modules..."
-
 npm install
 
 echo "Installing CocoaPods dependencies..."
-
 pod install --repo-update
